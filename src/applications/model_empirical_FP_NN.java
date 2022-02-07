e4900356f98786563f8bc17ae070cdb7360644bd
Lucas Degenhardt <lucas@teamwork-medien.de>
2022-02-07 18:50:04 +0100

    Model small fixes

--- src/applications/model_empirical_FP_NN.java
+++ src/applications/model_empirical_FP_NN.java
@@ -2,8 +2,10 @@ package applications;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -135,8 +137,37 @@ public class model_empirical_FP_NN {
 		System.out.println();
 	}
 
-	private void generateModel(){
-
+	public static void write(GeoPosition realPosition, GeoPosition estimatedPosition)
+	{
+		try {
+			File file = new File("empirical_FP_N.txt");
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+			else {
+				System.out.println("File already exists");
+			}
+			
+			
+			FileWriter fw = new FileWriter(file, true);
+			fw.write("RealPosition:"+realPosition);
+			fw.write(";");
+			fw.write("EstimatedPosition:"+estimatedPosition);
+			fw.write(System.getProperty( "line.separator" ));
+			fw.close();
+		}
+		catch(FileNotFoundException e)
+	    {
+	        System.out.println("File Not Found");
+	     }
+	    catch(IOException e)
+	    {
+	        System.out.println("something messed up");
+	    }
+	}
+	
+	public static void nearestNeighbour(TraceEntry oP, HashMap<GeoPosition, Double> eP) {
+		
 	}
 
 }
